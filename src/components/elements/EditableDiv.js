f364c2ca1180313d6cb47699f964e104614ede89
Luke Bayliss <lbayliss@scottlogic.com>
2021-11-30 17:46:28 +0000

    remote implementation - updating database on edit

--- src/components/elements/EditableDiv.js
+++ src/components/elements/EditableDiv.js
@@ -1,17 +1,28 @@
 import "bootstrap-icons/font/bootstrap-icons.css";
 import classes from "./EditableDiv.module.css";
-import { useEffect } from "react";
 
 function EditableDiv(props) {
-  const content = props.data.content;
+  function editPostHandler(postData) {
+    fetch(
+      `https://friendface-react-default-rtdb.europe-west1.firebasedatabase.app/posts/${props.data.id}.json`,
+      {
+        method: "PATCH",
+        body: JSON.stringify(postData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    // TODO: Feedback to the user that the post change was successful .then() ?
+  }
   return (
     <span
       contentEditable={true}
       suppressContentEditableWarning={true}
       className={classes.content}
-      onBlur={e => console.log('Text inside div', e.currentTarget.textContent)}
+      onBlur={(e) => editPostHandler({ content: e.currentTarget.textContent })}
     >
-      {content}
+      {props.data.content}
     </span>
   );
 }
