78a512011ac820b0e3a5fb0fe86a88ba79104690
Luke Bayliss <lbayliss@scottlogic.com>
2022-01-11 11:36:42 +0000

    No-logic filter box implementation

---
+++ src/components/elements/FilterBox.js
@@ -0,0 +1,12 @@
+import classes from "./FilterBox.module.css";
+
+function FilterBox() {
+  return (
+    <div>
+      <label>Filter content by: </label>
+      <input type="text"></input>
+    </div>
+  );
+}
+
+export default FilterBox;
